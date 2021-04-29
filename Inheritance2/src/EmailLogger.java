7bce82c7c96c391975f3fd6de6060a15c7383dbd
Mustafa <67148589+mustafasansar44@users.noreply.github.com>
2021-04-29 05:41:19 +0300

    Update EmailLogger.java

--- Inheritance2/src/EmailLogger.java
+++ Inheritance2/src/EmailLogger.java
@@ -1,7 +1,7 @@
 
 public class EmailLogger extends Logger{
-	@Override 		// Bunu yazmasakta log'u override eder ama yazarsak biz anlarýz.
+	@Override 		// Bunu yazmasakta log'u override eder
 	public void log() {
-		System.out.println("Email loglandý");
+		System.out.println("Email loglandÃ½");
 	}
 }
