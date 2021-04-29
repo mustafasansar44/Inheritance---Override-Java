15d24b52382d8116b2f6d91b237e911eba8cb667
Mustafa <67148589+mustafasansar44@users.noreply.github.com>
2021-04-29 05:42:04 +0300

    Update Main.java

--- Inheritance2/src/Main.java
+++ Inheritance2/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		CustomerManager customerManager = new CustomerManager();
-		customerManager.add(new DatabaseLogger());
+		customerManager.add(new DatabaseLogger());	// İstediğimiz loglama tekniğini burada belirtmemiz yetecektir.
 
 	}
 
